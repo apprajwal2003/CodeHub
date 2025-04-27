72a47ce68e15a21bc3349277058a020cc0333941
A P Prajwal <apprajwalwork@gmail.com>
2025-04-27 23:54:19 +0530

    server start

--- backend/controllers/revert.js
+++ backend/controllers/revert.js
@@ -1,3 +1,23 @@
-export async function revertRepo(params) {
-  console.log("Revert repo function called");
+import fs from "fs/promises";
+import path from "path";
+
+export async function revertRepo(commitId) {
+  try {
+    const repoPath = path.resolve(process.cwd(), ".codeHubGit");
+    const commitsPath = path.join(repoPath, "commits");
+
+    const parentDir = path.resolve(repoPath, "..");
+    const commitDir = path.join(commitsPath, commitId);
+    const files = await fs.readdir(commitDir);
+    for (const file of files) {
+      await fs.copyFile(
+        path.join(commitDir, file),
+        path.join(parentDir, file),
+        file.Body
+      );
+    }
+    console.log(`Reverted to commit: ${commitId}`);
+  } catch (error) {
+    console.log("Unable to revert!", error);
+  }
 }
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+import http from "http";
+import mongoose from "mongoose";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+dotenv.config();
+
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 import { initRepo } from "./controllers/init.js";
@@ -8,6 +17,7 @@ import { pullRepo } from "./controllers/pull.js";
 import { revertRepo } from "./controllers/revert.js";
 
 yargs(hideBin(process.argv))
+  .command("start", "Start the server", {}, startServer)
   .command("init", "Initialize a new repository", {}, initRepo)
   .command(
     "add <file>",
@@ -53,3 +63,21 @@ yargs(hideBin(process.argv))
   )
   .demandCommand(1, "You need to enter atleast one command")
   .help().argv;
+
+function startServer() {
+  const app = express();
+  const port = process.env.PORT || 8080;
+
+  app.use(bodyParser.json());
+  app.use(express.json());
+
+  const mongoURL = process.env.MONGO_URL;
+  mongoose
+    .connect(mongoURL)
+    .then(() => {
+      console.log("Connected to mongoDB successfully!");
+    })
+    .catch((error) => {
+      console.log("Unable to connect!", error);
+    });
+}
